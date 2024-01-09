c7a48ba2d3daec9648bae6dd66f8d1243f59cbc5
thititongumpun <thiti180536@gmail.com>
2024-01-09 22:25:10 +0700

    init

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System.Globalization;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using THHOLIDAYS;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
+builder.Services.AddBlazoredLocalStorageAsSingleton();
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("th-TH");
+
+await builder.Build().RunAsync();
